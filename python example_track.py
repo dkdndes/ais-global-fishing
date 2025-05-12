003e26be2d418e633d7aa62fc7041ffc018de6db
Peter Rosemann <dkdndes@gmail.com>
2025-05-12 17:54:01 +0200

    refactor: remove example scripts and update project configuration

--- python example_track.py
+++
@@ -1,7 +0,0 @@
-"""
-Documentation placeholder for the track example section that was
-accidentally committed as a *.py* file.
-
-All examples default to the MMSI 368045130 ("MISS FREYA").
-Tweak the parameters to explore the dataset of your choice.
-"""
